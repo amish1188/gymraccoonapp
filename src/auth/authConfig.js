b31b03ed3a636baf844728bb3085c1703f455e00
marcin <marcin.janecki@bouvet.no>
2023-03-18 20:00:57 +0100

    update

--- src/auth/authConfig.js
+++ src/auth/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
   clientId: '1813dd11-14f3-4fd3-a309-f3f0d78ce825',
   authority:
    'https://login.microsoftonline.com/3c8fef4a-d209-45a9-9810-18c2004dbb0d',
-  redirectUri: 'http://localhost:3000'
+  redirectUri: 'https://gymraccoonapp.azurewebsites.net/'
  },
  cache: {
   cacheLocation: 'sessionStorage', // This configures where your cache will be stored
