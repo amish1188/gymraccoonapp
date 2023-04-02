8a292fdcd737e0151c1230bea24c7b13c58acba1
marcin <marcin.janecki@bouvet.no>
2023-04-02 12:20:34 +0200

    added nav bar

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -1,26 +1,65 @@
-import { Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { Box, IconButton, Toolbar } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
-import MenuIcon from '@mui/icons-material/Menu';
+import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const NavigationBar = () => {
+ const navigate = useNavigate();
+ const location = useLocation();
+ const [locationTitle, setLocationTitle] = useState<string>('');
+
+ const goBackHandler = () => {
+  navigate(-1);
+ };
+
+ useEffect(() => {
+  switch (location.pathname) {
+   case '/':
+    setLocationTitle('User page');
+    break;
+   case '/training':
+    setLocationTitle('Your trainings');
+    break;
+   case '/current':
+    setLocationTitle('Current training');
+    break;
+   default:
+    setLocationTitle('Home');
+  }
+ }, [location.pathname]);
+
  return (
-  <Box>
+  <Box sx={{ flexGrow: 1 }}>
    <AppBar position='static'>
     <Toolbar>
-     <IconButton
-      size='large'
-      edge='start'
-      color='inherit'
-      aria-label='menu'
-      sx={{ mr: 2 }}
+     {location.pathname !== '/' && (
+      <IconButton
+       style={{ position: 'absolute', zIndex: '100' }}
+       size='large'
+       edge='start'
+       color='inherit'
+       aria-label='menu'
+       sx={{ mr: 2 }}
+       onClick={goBackHandler}
+      >
+       <ArrowBackIosRoundedIcon />
+      </IconButton>
+     )}
+     <h3
+      style={{
+       position: 'absolute',
+       fontWeight: '500',
+       margin: 'auto',
+       left: '0',
+       width: '100%',
+       zIndex: '0',
+       textAlign: 'center'
+      }}
      >
-      <MenuIcon />
-     </IconButton>
+      {locationTitle}
+     </h3>
     </Toolbar>
-    <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-     News
-    </Typography>
-    <Button color='inherit'>Login</Button>
    </AppBar>
   </Box>
  );
