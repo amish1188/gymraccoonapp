635f5d8abc39ec6cf7aa188c56a7a2d7f625211d
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2023-04-04 16:59:14 +0200

    add config (#3)

--- src/auth/authConfig.ts
+++ src/auth/authConfig.ts
@@ -2,7 +2,8 @@ export const msalConfig = {
  auth: {
   clientId: process.env.REACT_APP_CLIENT_ID as string,
   authority: process.env.REACT_APP_AUTHORITY as string,
-  redirectUri: 'http://localhost:3000'
+  redirectUri: process.env.REACT_APP_REDIRECT_URI
+  // 'http://localhost:3000'
  },
  cache: {
   cacheLocation: 'sessionStorage', // This configures where your cache will be stored
